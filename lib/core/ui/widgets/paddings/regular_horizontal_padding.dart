8635d9dc414ef609faffd86637012477232a4823
marcelmarchetti <marchettimarcel9@gmail.com>
2025-01-10 00:37:00 +0100

    feat: añadido el valor del padding regular al fixero de constantes Values

--- lib/core/ui/widgets/paddings/regular_horizontal_padding.dart
+++ lib/core/ui/widgets/paddings/regular_horizontal_padding.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../../../constants/values.dart';
+
 class RegularHorizontalPadding extends StatelessWidget {
   const RegularHorizontalPadding({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 17)
+        padding: EdgeInsets.symmetric(horizontal: Values.horizontalPadding)
     );
   }
 }
