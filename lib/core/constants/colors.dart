8e5e7cf1152a7bd3e662c65b939ff9a0a4507418
marcelmarchetti <marchettimarcel9@gmail.com>
2025-01-10 01:54:42 +0100

    feat: añadida paleta de colores

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Color (Azul)
+  static const Color primary = Color(0xFF0056A0);
+  static const Color primaryContainer = Color(0xFFB0D1F7);
+  static const Color onPrimaryContainer = Color(0xFF003366);
+
+  // Secondary Color (Azul más claro)
+  static const Color secondary = Color(0xFF007BFF);
+  static const Color secondaryContainer = Color(0xFFD6E4FF);
+  static const Color onSecondaryContainer = Color(0xFF003366);
+
+  // Tertiary Color (Blanco)
+  static const Color tertiary = Color(0xFFFFFFFF);
+  static const Color tertiaryContainer = Color(0xFFF1F1F1);
+  static const Color onTertiaryContainer = Color(0xFF000000);
+
+  // Otros colores
+  static const Color background = Color(0xFFF4F4F4);
+  static const Color onBackground = Color(0xFF000000);
+}
