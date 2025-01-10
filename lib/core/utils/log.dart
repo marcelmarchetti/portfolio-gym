0e6c39ab68780c4627a45d114e3e27f4017dedf3
marcelmarchetti <marchettimarcel9@gmail.com>
2025-01-10 01:27:29 +0100

    feat: añadido logger

---
+++ lib/core/utils/log.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+
+class Log {
+  static void info(String message) {
+    if (kDebugMode) {
+      print("INFO: $message");
+    }
+  }
+
+  static void warning(String message) {
+    if (kDebugMode) {
+      print("WARNING: $message");
+    }
+  }
+
+  static void error(String message) {
+    if (kDebugMode) {
+      print("ERROR: $message");
+    }
+  }
+}
