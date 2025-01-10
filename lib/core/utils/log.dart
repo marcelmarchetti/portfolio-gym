096a7cdd42326099979b350acf45c57f46553640
marcelmarchetti <marchettimarcel9@gmail.com>
2025-01-10 01:45:47 +0100

    feat: añadido colores al logging

--- lib/core/utils/log.dart
+++ lib/core/utils/log.dart
@@ -3,26 +3,26 @@ import 'package:flutter/foundation.dart';
 class Log {
   static void info(String message) {
     if (kDebugMode) {
-      print("INFO: $message");
+      print('\x1B[38;5;33mINFO: $message\x1B[0m');
     }
   }
 
   static void warning(String message) {
     if (kDebugMode) {
-      print("WARNING: $message");
+      print('\x1B[33mWARNING: $message\x1B[0m');
     }
   }
 
   static void error(String message) {
     if (kDebugMode) {
-      print("ERROR: $message");
+      print('\x1B[31mERROR: $message\x1B[0m');
     }
   }
 
   static void errorWithStackTrace(Object message, StackTrace stackTrace) {
     if (kDebugMode) {
-      print("ERROR: ${message.toString()}");
-      print("STACKTRACE: $stackTrace");
+      print('\x1B[31mERROR: ${message.toString()}\x1B[0m');
+      print('\x1B[31mSTACKTRACE: $stackTrace\x1B[0m');
     }
   }
 }
