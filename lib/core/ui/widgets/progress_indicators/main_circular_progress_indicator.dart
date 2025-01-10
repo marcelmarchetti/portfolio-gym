ea16d5a9f334a97bd916a378eab77810ee5ab2fb
marcelmarchetti <marchettimarcel9@gmail.com>
2025-01-10 02:04:39 +0100

    feat: añadido indicador de carga principal

---
+++ lib/core/ui/widgets/progress_indicators/main_circular_progress_indicator.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants/colors.dart';
+
+class MainCircularProgressIndicator extends StatefulWidget {
+  const MainCircularProgressIndicator({super.key});
+
+  @override
+  State<MainCircularProgressIndicator> createState() => _MainCircularLoadingIndicatorState();
+}
+
+class _MainCircularLoadingIndicatorState extends State<MainCircularProgressIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    return CircularProgressIndicator(
+      color: AppColors.primary,
+      backgroundColor: Colors.transparent,
+      strokeWidth: 2,
+    );
+  }
+}
